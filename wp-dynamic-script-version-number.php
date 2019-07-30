97abc77a1b94c732b91eb2278dedaaeb62f48757
Md Kowsar Hossain <techlabpro8@gmail.com>
2019-07-30 11:57:42 +0600

    first commit

---
+++ wp-dynamic-script-version-number.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: WP Dynamic Script Version Number
+Plugin URI: http://radiustheme.com
+Description: Generate Dynamic Script Version Number into CSS/JS to ignore static cache
+Version: 1.0
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Change asset version num
+add_filter( 'script_loader_src', 'foa_dynamic_script_ver', 150 );
+add_filter( 'style_loader_src', 'foa_dynamic_script_ver', 150);
+function foa_dynamic_script_ver( $src ){
+	return add_query_arg( 'ver', time(), $src );
+}
